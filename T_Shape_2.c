69f05372683dd6bb9717286833d615a10700273e
safayatalif <safayatalif2021@gmail.com>
2025-11-05 17:55:43 +0600

    4 problem solved

---
+++ T_Shape_2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int star = 1;
+    int spece = n - 1;
+    for (int i = 1; i <= n; i++) { 
+        for (int s = spece; s >= 1; s--) {
+            printf(" ");
+        }
+        for (int j = 1; j <= star; j++) {
+            printf("*");
+        }
+        printf("\n");
+        star = star + 2;
+        spece--;
+    }
+    return 0;
+}
